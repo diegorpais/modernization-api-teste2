8dd68f6cb841e4a02ab1775d44ef0556cb4b0fe6
Diego Pais <83249824+diegorpais@users.noreply.github.com>
2025-02-24 16:23:48 -0300

    Modernization Code: teste212121, File: consolidated-frontend.zip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,10 @@
 import { Component } from '@angular/core';
-import { NotificationService } from './notification/notification.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'seu-nome-do-app';
-
-  constructor(private notificationService: NotificationService) { }
-
-  triggerNotification() {
-    // Substitui o uso de alert por notificação
-    // alert('teste');
-    this.notificationService.showNotification('teste');
-  }
+  title = 'Sum Application';
 }
